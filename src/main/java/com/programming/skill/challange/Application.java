79d97b7b9575ad0fd23e7fa9663fd40cf9e6d89e
allanbfra <allan.ferreira@outsmartdigital.com.br>
2022-04-03 16:54:03 -0300

    [FEAT] adjustments

--- src/main/java/com/programming/skill/challange/Application.java
+++ src/main/java/com/programming/skill/challange/Application.java
@@ -4,12 +4,15 @@ import com.programming.skill.challange.core.usecases.SecondQuestion;
 import com.programming.skill.challange.core.usecases.TenthQuestion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class Application {
-
+	private static final Logger LOGGER = Logger.getLogger(Application.class.getName());
+	
 	public static void main(String[] args) {
 		SecondQuestion.execute();
-		TenthQuestion.reverseString("allan");
+		LOGGER.info("TenthQuestion: " + TenthQuestion.reverseString("allan"));
 	}
 
 }
--- src/main/java/com/programming/skill/challange/core/usecases/TenthQuestion.java
+++ src/main/java/com/programming/skill/challange/core/usecases/TenthQuestion.java
@@ -1,10 +1,6 @@
 package com.programming.skill.challange.core.usecases;
 
-import java.util.logging.Logger;
-
 public class TenthQuestion {
-    private static final Logger LOGGER = Logger.getLogger(TenthQuestion.class.getName());
-
     public static String reverseString(String string) {
         for (int i = string.length() - 1; i >= 0; --i) {
             string += string.charAt(i);
